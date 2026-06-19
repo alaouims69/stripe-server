63c7855e3de56cc6031f915814f2d0b37779edb5
Alaoui <alaouims69@gmail.com>
2026-06-19 08:28:00 +0200

    V9 — mise à jour APP_VERSION

--- server.py
+++ server.py
@@ -9,8 +9,8 @@ ASSOCIATIONS = {
 }
 
 APP_VERSION = {
-    "versionCode": 7,
-    "apkUrl": "https://github.com/alaouims69/stripe-server/releases/download/V7/app-release.apk"
+    "versionCode": 9,
+    "apkUrl": "https://github.com/alaouims69/MosqueeCollecte/releases/download/V9/app-release.apk"
 }
 
 class Handler(BaseHTTPRequestHandler):
